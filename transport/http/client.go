9e31178f11560844e52c52bac2faf66d97015343
psmarcin <ps@psmarcin.me>
2022-09-23 10:46:22 +0200

    Disable request body close

--- transport/http/client.go
+++ transport/http/client.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"net/http"
 
-	smithy "github.com/aws/smithy-go"
+	"github.com/aws/smithy-go"
 	"github.com/aws/smithy-go/middleware"
 )
 
@@ -75,9 +75,9 @@ func (c ClientHandler) Handle(ctx context.Context, input interface{}) (
 	// HTTP RoundTripper *should* close the request body. But this may not happen in a timely manner.
 	// So instead Smithy *Request Build wraps the body to be sent in a safe closer that will clear the
 	// stream reference so that it can be safely reused.
-	if builtRequest.Body != nil {
-		_ = builtRequest.Body.Close()
-	}
+	// if builtRequest.Body != nil {
+	//   _ = builtRequest.Body.Close()
+	// }
 
 	return &Response{Response: resp}, metadata, err
 }
